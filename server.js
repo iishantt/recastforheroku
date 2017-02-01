fe18a153f7199fc309b2c1f136ad073be55b0756
geekvaibhav <vaibhav.singh144@gmail.com>
2017-02-01 16:21:35 +0530

    first commit to upload

---
+++ server.js
@@ -0,0 +1,41 @@
+/* module imports */
+const BotConnector = require('recastai-botconnector')
+const recastai = require('recastai')
+const express = require('express')
+const bodyParser = require('body-parser')
+
+/* Bot Connector connection */
+const myBot = new BotConnector({ userSlug: 'geekvaibhav', botId: '588b3523e807fe2c951faa2f', userToken: 'f7e0e721c3a1de2efb5378f8e7044b73' })
+
+/* Recast.AI API connection */
+const client = new recastai.Client('a7a706351177d232d7c1efca6cb6e9d6')
+
+/* Server setup */
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json())
+app.post('/', (req, res) => myBot.listen(req, res))
+app.listen(port, () => console.log('Bot running on port', port))
+
+/* When a bot receive a message */
+myBot.onTextMessage(message => {
+  console.log(message)
+  const userText = message.content.attachment.content
+  const conversationToken = message.senderId
+
+  client.textConverse(userText, { conversationToken })
+    .then(res => {
+      // We get the first reply from Recast.AI or a default reply
+      const reply = res.reply() || 'Sorry, I didn\'t understand'
+
+      const response = {
+        type: 'text',
+        content: reply,
+      }
+
+      return message.reply(response)
+    })
+    .then(() => console.log('Message successfully sent'))
+    .catch(err => console.error(`Error while sending message: ${err}`))
+})
